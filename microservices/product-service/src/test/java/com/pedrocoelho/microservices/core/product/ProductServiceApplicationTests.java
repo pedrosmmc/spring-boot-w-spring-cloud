a18c5a4a4bf897d34cab52ea3ec149c0da9ff212
Pedro Coelho <pedrosmmc@gmail.com>
2022-04-10 15:44:25 +0100

    Microservices skeleton code cleanup

--- microservices/product-service/src/test/java/com/pedrocoelho/microservices/core/product/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/com/pedrocoelho/microservices/core/product/ProductServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.pedrocoelho.microservices.core.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
